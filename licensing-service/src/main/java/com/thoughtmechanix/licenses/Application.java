8025fcab8ef967b67838359f8f6497c31a45ede5
Trifon <trifont@gmail.com>
2017-07-13 08:52:26 +0300

    Removed minor WARNING.

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -1,6 +1,5 @@
 package com.thoughtmechanix.licenses;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
